0f23afceb42bea9159e2bf17e26c8ed1e86451a3
NanditaReji-STUDENT <g00463584@atu.ie>
2025-12-03 16:37:43 +0000

    Initial changes

--- src/main/java/ie/atu/productv3/ProductApp.java
+++ src/main/java/ie/atu/productv3/ProductApp.java
@@ -16,10 +16,12 @@ public class ProductApp {
             System.out.print("Enter product code: ");
             String productCode = sc.nextLine();  // read the product code
 
+            Product p= ProductDB.getProduct(productCode);
+
             // Book or a Software Object? = ProductDB.getProduct(productCode);
 
             // display the output
-            /*
+
             System.out.println();
             if (p != null) {
                 System.out.println("Description: " + p.toString());
@@ -35,7 +37,7 @@ public class ProductApp {
             System.out.print("Continue? (y/n): ");
             choice = sc.nextLine();
             System.out.println();
-            */
+
         }
     }
 }
--- src/main/java/ie/atu/productv3/ProductDB.java
+++ src/main/java/ie/atu/productv3/ProductDB.java
@@ -1,12 +1,62 @@
 package ie.atu.productv3;
 
+
 public class ProductDB {
 
-    //  public static Book or Software getProduct(String productCode) {
-    // In a more realistic application, this code would get the data for the product from a file or database
-    // For now, this code just uses if/else statements to return the correct product data
+    public static Product getProduct(String productCode) {
+        Book myBook = null;
+        Software mySoftware = null;
+        Product myProduct = null;
 
-    //We need all the book and software objects, but what kind of object do we return?
-    //}
+        if (productCode.equalsIgnoreCase("java")) {
+            myBook = new Book();
+            myBook.setCode(productCode);
+            myBook.setDescription("ATU Java Programming");
+            myBook.setPrice(57.50);
+            myBook.setAuthor("Joe Brown");
+            myProduct= myBook;
+
+        } else if (productCode.equalsIgnoreCase("jsp")) {
+            myBook = new Book();
+            myBook.setCode(productCode);
+            myBook.setDescription("Java Servlets and JSP");
+            myBook.setPrice(57.50);
+            myBook.setAuthor("Mike White");
+            myProduct= myBook;
+        } else if (productCode.equalsIgnoreCase("mysql")) {
+            myBook = new Book();
+            myBook.setCode(productCode);
+            myBook.setDescription("Lennon's MySQL");
+            myBook.setPrice(54.50);
+            myBook.setAuthor("Jim Lennon");
+            myProduct= myBook;
+        }
+
+        if (productCode.equalsIgnoreCase("studios")) {
+            mySoftware = new Software();
+            mySoftware.setCode(productCode);
+            mySoftware.setDescription("Visual Studios");
+            mySoftware.setPrice(57.50);
+            mySoftware.setVersion("Microsoft 1.1");
+            myProduct= mySoftware;
+        } else if (productCode.equalsIgnoreCase("eclipse")) {
+            mySoftware = new Software();
+            mySoftware.setCode(productCode);
+            mySoftware.setDescription("Build Java apps");
+            mySoftware.setPrice(57.50);
+            mySoftware.setVersion("Eclipse Neon");
+            myProduct= mySoftware;
+        } else if (productCode.equalsIgnoreCase("oracle")) {
+            mySoftware = new Software();
+            mySoftware.setCode(productCode);
+            mySoftware.setDescription("Latest MySQL");
+            mySoftware.setPrice(54.50);
+            mySoftware.setVersion("Oracle 3.0");
+            myProduct= mySoftware;
+        }
+
+        return myProduct;
+
+    }
+
 }
-
